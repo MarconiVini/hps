3134a1a2ed6d9b33eeeef48318fbdca5e8e92b44
Marcos Vinícius Cardoso Santini <msantini@ciandt.com>
2020-05-29 18:48:17 -0300

    removing sizes

--- db/migrate/20200326225550_create_convenios.rb
+++ db/migrate/20200326225550_create_convenios.rb
@@ -2,12 +2,12 @@ class CreateConvenios < ActiveRecord::Migration[5.1]
   def change
     create_table :convenios do |t|
       t.string :cnpj
-      t.string :razao_social, limit: 15
-      t.string :fantasia, limit: 50
+      t.string :razao_social, limit: 100
+      t.string :fantasia, limit: 100
       t.string :estado, limit: 2
       t.string :cep, limit: 8
-      t.string :fone1, limit: 12
-      t.string :fone2, limit: 12
+      t.string :fone1, limit: 20
+      t.string :fone2, limit: 20
       t.string :email, limit: 120
       t.boolean :ativo
 
