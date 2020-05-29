3935488462c88ba2909db45cacd7ca90f640f544
Marcos Vinícius Cardoso Santini <msantini@ciandt.com>
2020-05-29 19:09:59 -0300

    changed value size

--- db/migrate/20200326225550_create_convenios.rb
+++ db/migrate/20200326225550_create_convenios.rb
@@ -2,12 +2,12 @@ class CreateConvenios < ActiveRecord::Migration[5.1]
   def change
     create_table :convenios do |t|
       t.string :cnpj
-      t.string :razao_social, limit: 100
-      t.string :fantasia, limit: 100
-      t.string :estado, limit: 2
-      t.string :cep, limit: 8
-      t.string :fone1, limit: 20
-      t.string :fone2, limit: 20
+      t.string :razao_social, limit: 200
+      t.string :fantasia, limit: 200
+      t.string :estado, limit: 100
+      t.string :cep, limit: 100
+      t.string :fone1, limit: 100
+      t.string :fone2, limit: 100
       t.string :email, limit: 120
       t.boolean :ativo
 
--- db/migrate/20200326225849_create_vagas.rb
+++ db/migrate/20200326225849_create_vagas.rb
@@ -1,8 +1,8 @@
 class CreateVagas < ActiveRecord::Migration[5.1]
   def change
     create_table :vagas do |t|
-      t.string :quadra, limit: 10
-      t.string :tipo, limit: 15
+      t.string :quadra, limit: 200
+      t.string :tipo, limit: 200
 
       t.timestamps
     end
